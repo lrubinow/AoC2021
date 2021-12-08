3347459f184e29517c36811292bd54eb6915e521
Larry Rubinow <larry.rubinow@daugherty.com>
2021-12-08 12:03:17 -0600

    explanatory comments

--- src/main/groovy/com/rubinow/challenge08/Challenge8b.groovy
+++ src/main/groovy/com/rubinow/challenge08/Challenge8b.groovy
@@ -40,11 +40,17 @@ class ChallengeB {
         Set<String> seven = filterByCount(line, 3)[0].toSet()
         Set<String> four = filterByCount(line, 4)[0].toSet()
         Set<String> eight = filterByCount(line, 7)[0].toSet()
+        // zero has six elements, contains all elements of one, but doesn't contain all elements of four
         Set<String> zero = filterByCount(line, 6).toList().stream().filter(x -> (x.toSet().containsAll(one) && !(x.toSet().containsAll(four)))).collect().getAt(0).toString().toSet()
+        // nine has six elements, doesn't contain all elements of zero, but does contain all elements of one
         Set<String> nine = filterByCount(line, 6).toList().stream().filter( x -> { !(x.toSet().containsAll(zero)) && (x.toSet().containsAll(one))}).collect().getAt(0).toString().toSet()
+        // six has six elements, but is not zero or nine
         Set<String> six = filterByCount(line, 6).toList().stream().filter( x -> { !(x.toSet().containsAll(zero)) && !(x.toSet().containsAll(nine))}).collect().getAt(0).toString().toSet()
+        // three has five elements, contains all elements of one
         Set<String> three = filterByCount(line, 5).toList().stream().filter(x -> (x.toSet().containsAll(one))).collect().getAt(0).toString().toSet()
+        // five has five elements, all of which are contained in six
         Set<String> five = filterByCount(line, 5).toList().stream().filter(x -> six.containsAll(x.toSet())).collect().getAt(0).toString().toSet()
+        // two has five elements, but is not three or five
         Set<String> two = filterByCount(line, 5).toList().stream().filter( x -> { !(x.toSet().containsAll(three)) && !(x.toSet().containsAll(five))}).collect().getAt(0).toString().toSet()
 
         Map<Set<String>, Integer> decoder = new HashMap<Set<String>, Integer>()
